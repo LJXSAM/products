58ebd9391da42fe62f8dfde1f4e9b357b59b7c2d
LJXSAM <47037294+LJXSAM@users.noreply.github.com>
2019-02-12 11:14:50 -0500

    Add files via upload

--- products.py
+++ products.py
@@ -1,15 +1,30 @@
+#读取档案
 products = []
-while True:
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,价格'in line:
+			continue
+		namee, price = line.split().split(',')
+		products.qppend([name, price ])
+
+print(products)
+
+
+#让使用者输入
 	name = input('请输入商品名称:')
 	if name == 'q':
 		break 
-	price = input('请输入价格：')   
+	price = input('请输入价格：') 
+	price = int(price)  
 	products.append([name,  price])
 print(products)
 
+#印出所有购买记录
 for p in products:
 	print(p[0], '的价格是', p[1])
 
-with open ('products.csv ','w') as f :
+#写入档案
+with open ('products.csv ','w', encoding = 'utf-8') as f :
+	f.write('商品,价格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write (p[0] + ',' + str(p[0]) + '\n')
